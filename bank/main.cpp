e2d53141336f6774557e53658bdd46a84441112d
Phil Lentz <Phillip.Lentz@yahoo.com>
2019-06-07 17:47:57 -0400

    test

--- bank/main.cpp
+++ bank/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <typeinfo>
-#include "person.h"
+#include "Person.h"
+#include "Admin.h"
 #include "teller.h"
 #include "logger.h"
-#include "menu.h"
+//#include "menu.h"
 
 using namespace std;
 
@@ -12,22 +13,41 @@ void audit();
 int main()
 {
     Person p1("test test", "55/55/5555", "testname", "testpass");
-    Teller t1("test test", "55/55/5555", "testname", "testpass", 18.5);
-//    //Person p1;
-//    cout << p1.getDetails() << endl
+    Teller t1("test test", "66/66/6666", "testname", "testpass", 18.5);
+    Admin  a1("test test", "77/77/7777", "testname", "testpass", 18.5);
+    //Client c1("test test", "88/88/8888", "testname", "testpass", 18.5);
+    //Person p1;
+    cout << p1.getDetails() << endl
+         << endl
+         << t1.getDetails() << endl;
+
+    cout << p1.getName() << endl
+         << endl
+         << t1.getName() << endl;
+         
+    cout << a1.getName() << endl
+         << endl
+         << a1.getName() << endl;
+         
+//    cout << c1.getName() << endl
 //         << endl
-//         << t1.getDetails() << endl;
-//
-//    cout << p1.getName() << endl
-//         << endl
-//         << t1.getName() << endl;
-//
-//    t1.setName("New Name");
-//    p1.setName("NEW NAME");
-//
-//    cout << p1.getDetails() << endl
-//         << endl
-//         << t1.getDetails() << endl;
+//         << c1.getName() << endl;
+
+    t1.setName("New Name");
+    p1.setName("NEW NAME");
+    p1.setName("NEW NAME");
+    p1.setName("NEW NAME");
+	p1.setName("NEW NAME");
+    a1.setName("NEW NAME");
+    //c1.setName("NEW NAME");
+
+    cout << p1.getDetails() << endl
+         << endl
+         << t1.getDetails() << endl
+         << endl
+         << a1.getDetails() << endl
+         << endl;
+         //<< c1.getDetails() << endl;
 
     // Polymorphism
 //    Person* ps[2];
@@ -39,19 +59,19 @@ int main()
 //        cout << endl;
 //    }
 
-    Logger mainLog("test");
-    mainLog.log("it wednesday my dudes");
-    Logger adminLog("Admin_Log");
-    adminLog.log("There was an error\tmy\ndude!");
-    adminLog.log("jeiabjfbrjajdibn");
+//    Logger mainLog("test");
+//    mainLog.log("it wednesday my dudes");
+//    Logger adminLog("Admin_Log");
+//    adminLog.log("There was an error\tmy\ndude!");
+//    adminLog.log("jeiabjfbrjajdibn");
 
 
-    Menu main("Main Menu");
+   /* Menu main("Main Menu");
     main.addOption("Audit", audit);
     main.addOption("add", audit);
     main.addOption("there", audit);
     while(1)
-        main.run();
+        main.run(); */
 }
 
 void audit(){
